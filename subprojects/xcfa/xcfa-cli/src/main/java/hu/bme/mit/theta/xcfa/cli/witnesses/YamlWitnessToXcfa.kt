b389571b9bfe22954641d8f845428092f8a09087
Klevis Imeri <klevisimeri11@gmail.com>
2025-03-19 18:44:38 +0100

    No havoc but AssumeSmt at return and answers to questions

--- subprojects/xcfa/xcfa-cli/src/main/java/hu/bme/mit/theta/xcfa/cli/witnesses/YamlWitnessToXcfa.kt
+++ subprojects/xcfa/xcfa-cli/src/main/java/hu/bme/mit/theta/xcfa/cli/witnesses/YamlWitnessToXcfa.kt
@@ -10,8 +10,23 @@ import hu.bme.mit.theta.c2xcfa
 import hu.bme.mit.theta.frontend.ParseContext
 import hu.bme.mit.theta.core.stmt.AssumeStmt
 
-// TODO: Are edges to itself o/w added?
-class YamlWitnessToXcfa(witness: YamlWitness, program: XCFA, source: String, logger: Logger) {
+// Work from main
+
+//  \return ? == 2
+//         XXXX not like this
+// [] -- int x = havoc() --> []
+// [] --  x == 2 --> [] you dont have to put an havoc statement but you need a AsumeSmt
+// HavocPromotionAndRange :42
+// Execute config :172
+
+// TODO: remove source
+// TODO: Are edges to itself o/w added? Yes
+class YamlWitnessToXcfa(
+  witness: YamlWitness, 
+  program: XCFA, 
+  parseContext: ParseContext, 
+  logger: Logger
+) {
   
   val xcfaBuilder: XcfaBuilder; 
   val mainProcBuilder: XcfaProcedureBuilder;
@@ -24,7 +39,7 @@ class YamlWitnessToXcfa(witness: YamlWitness, program: XCFA, source: String, log
     locationMap = mutableMapOf<Location, XcfaLocation>()
     mainProcBuilder = XcfaProcedureBuilder("main", ProcedurePassManager()) 
     mainProcBuilder.createInitLoc()
-    // mainProcBuilder.createFinalLoc() TODO: Do we really need a final locaition for procedure?
+    // mainProcBuilder.createFinalLoc() TODO: Only for Correctnes
     currentLoc = mainProcBuilder.initLoc
     return when (witness.entryType) {
       EntryType.VIOLATION -> violationWitnessToXcfa()
@@ -70,7 +85,7 @@ class YamlWitnessToXcfa(witness: YamlWitness, program: XCFA, source: String, log
             mainProcBuilder.addEdge(XcfaEdge(
               currentLoc,
               targetLoc,
-              exp,
+              AssumeStmt(stmt),
               EmptyMetaData
             ))
             currentLoc = targetLoc
@@ -83,7 +98,7 @@ class YamlWitnessToXcfa(witness: YamlWitness, program: XCFA, source: String, log
           }
         }
       }
-
+        
       WaypointType.TARGET -> {
         if (action == Action.FOLLOW) {
           val errorLoc =  addLocation(location, true);
@@ -99,8 +114,11 @@ class YamlWitnessToXcfa(witness: YamlWitness, program: XCFA, source: String, log
         }
       }
 
-      WaypointType.FUNCTION_ENTER -> {} //ignore TODO: What about avoid fucntion enter?
-
+      WaypointType.FUNCTION_ENTER -> {} //ignore
+      
+      // Thete when parses it can inline funcions so not all funcions are supported
+      // We have to through a warning in this case. But we have havocs. 
+      // At the begging you have more informatoin 
       WaypointType.FUNCTION_RETURN -> {
         val targetLoc = newLocation(location);
         when (action) {
@@ -110,14 +128,14 @@ class YamlWitnessToXcfa(witness: YamlWitness, program: XCFA, source: String, log
               mainProcBuilder.addEdge(XcfaEdge(
                 currentLoc 
                 targetLoc,
-                HavocStmt.of(getVarDeclAtLocation(Location)); // TODO: is this the right way to creat havoc
+                AssumeStmt(Exp<BoolExpr>) // TODO: I have to get it from value and variables in XCFA
                 EmptyMetaData,
               ))
               currentLoc = targetLoc
             }
           }
           Action.AVOID -> {
-            // toTrapNode(currentLoc, ?)  TODO: what type of label
+            // toTrapNode(currentLoc, ?)  TODO: what type of label ...
           }
           else -> {
               throw IllegalArgumentException("Unknown action type: $action")
@@ -153,22 +171,23 @@ class YamlWitnessToXcfa(witness: YamlWitness, program: XCFA, source: String, log
     }
   }
 
-
-// Idea is that you create a label where you have the exact location and the statement.
-// This is the same for all types of statemetns.
-fun CorrectnessWitnessesToXcfa(witness: YamlWitness): XCFA {
+  // Idea is that you create a label where you have the exact location and the statement.
+  // This is the same for all types of statemetns.
+  fun CorrectnessWitnessesToXcfa(witness: YamlWitness): XCFA {
     witness.content.forEach { contentItem ->
-      contentItem.segment?.let { invariant ->
-        // segment.forEach { waypoint ->
-          val (type, constraint, location, action) = waypoints.waypoint
-          if(forma2)
-        // }
+      val (type, location, value, format) = contentItem.
+      if(format != C_EXPRESSION) {
+        throw IllegalArgumentException("Only  C_EXPRESSION is supported currently")
       }
+      val targetLoc = newLocation(location);
+      val assumeStmt = CExpToAssumeStmt(value);
+      
+
     }
 
     xcfaBuilder.addProcedure(mainProcBuilder)
     xcfaBuilder.addEntryPoint(mainBuilder, emptyList())
-}
+  }
 
 
   private fun newLocation(location: Location, error: Boolean = false): XcfaLocation {
@@ -192,68 +211,26 @@ fun CorrectnessWitnessesToXcfa(witness: YamlWitness): XCFA {
     )
   }
   
-  // TODO: Understand thsi fucntion exacly
+  //TODO: We dont even need this now
   private fun CExpToAssumeStmt(value: String): AssumeStmt {
-    val parseContext = ParseContext() //TODO: Understand what does this do and how is it used below?
+    val parseContext = ParseContext()
     val proc = program.initProcedures.first().first
     val ifBeforeReachErrorCall = proc.edges.filter {
-      it.getCMetaData()?.let { 
-          it.lineNumberStart == location.line && it.colNumberStart == location.column 
+      it.getCMetaData()?.let { // TODO: This si worng int between colum does not matter  
+          it.lineNumberStart <= location.line && location.line <= it.lineNumberStop &&
+          it.colNumberStart <= location.column  && location.column <= it.colNumerStop
       } ?: false 
     }.first()
-
+    
+    // THe scope definintions are ...
     val exp = parseCExpression(
         value,
         vars = this.program.collectVars().associateWith { CComplexType.getType(it.ref, parseContext) },
-        scope = ifBeforeReachErrorCall.getCMetaData()!!.scope.reversed(),
+        scope = ifBeforeReachErrorCall.getCMetaData()!!.scope.reversed(), // TODO: check with DB
         warningLogger = logger
     )
 
-    return AssumeStmt.of(exp as Expr<BoolType>) //TODO: can you do it like this?
-  }
-  
-  private getVarDeclAtLocation(location: Location) {
-    // TODO: Use the XCFA.initProcedureBuildrs.first().vars ?
-    // Do something similar to parseCExpression exmaple
-  }
-
-  // TODO: Problamatic if not in the same line
-  private fun getNameOfFunctionAtLocation(location: Location): String {
-    val lines = try {
-      File(source).takeIf { it.exists() }?.readLines() 
-          ?: source.split("\n")
-    } catch (e: Exception) {
-      logger.error("Error reading source: ${e.message}")
-      throw IllegalArgumentException("Yaml witness location is not appropiate for the input file!")
-    }
-
-    var (line, column, function) = location;
-    line--
-    column-- 
-
-    if (lineNumber !in lines.indices) throw IllegalArgumentException("Yaml witness location is not appropiate for the input file!")
-    if (columnNumber !in line.indices) throw IllegalArgumentException("Yaml witness location is not appropiate for the input file!")
-    if (lines[line][column] != ')') throw IllegalArgumentException("Yaml witness location is not appropiate for the input file!")
-
-    var parenCount = 0;
-    var pos = column; 
-      while (pos >= 0 && parenCount > 0) {
-        when (line[pos]) {
-          ')' -> parenCount++
-          '(' -> parenCount--
-        }
-        pos--
-      }
-
-    if (parenCount != 0) throw IllegalArgumentException("Yaml witness location is not appropiate for the input file!")
-
-    var nameStart = pos
-    while (nameStart >= 0 && line[nameStart].isWhitespace()) {
-      nameStart--
-    }
-
-    val functionName = line.substring(nameStart + 1, pos).trim()
-    return functionName.ifEmpty { "unknown" }
+    return AssumeStmt.of(exp as Expr<BoolType>)
   }
 
 }
